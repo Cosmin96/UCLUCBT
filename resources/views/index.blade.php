03e3a6ea9ebb8c92a998eda8c414268b99d64ad0
Cosmin Nicolae <marnicos2006@yahoo.com>
2016-11-24 20:20:06 +0000

    Initial Laravel setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v3.9.2, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:700,400&amp;subset=cyrillic,latin,greek,vietnamese">
+  <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/animate.css/animate.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/mobirise/css/style.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/mobirise/css/mbr-additional.css')}}" type="text/css">
+  
+  
+  
+</head>
+<title>UCLU CBT</title>
+<body>
+
+<div id="logo">
+<section class="engine"><a rel="external" href="https://mobirise.com">simple website maker</a></section><section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height mbr-section--bg-adapted" id="header1-0" style="background-color: rgb(0, 0, 0);">
+    <div class="mbr-box__magnet mbr-box__magnet--sm-padding mbr-box__magnet--center-left mbr-after-navbar">
+        
+        <div class="mbr-box__container mbr-section__container container">
+
+            <div class="mbr-box mbr-box--stretched">
+                <div class="row"><div class=" col-sm-6 col-sm-offset-6">
+                  <div class="mbr-box__magnet mbr-box__magnet--center-left">
+                    <div class="mbr-hero animated fadeInUp">
+                        <h1 class="mbr-hero__text">BLOCKCHAIN<br><span style="font-weight: normal;">LEADING THE NEW REVOLUTION</span></h1>
+                        <p class="mbr-hero__subtext"></p>
+                    </div>
+                    <div class="mbr-buttons btn-inverse mbr-buttons--left"><a class="mbr-buttons__btn btn btn-lg animated fadeInUp delay btn-info" href="#" onClick="researchPage()">Research</a> 
+                    <a class="mbr-buttons__btn btn btn-lg btn-default animated fadeInUp delay" href="#" onClick="studentsPage()">Students</a>
+                  </div>
+                </div></div>
+            </div></div>
+        </div>
+    </div>
+</section>
+</div>
+
+<div id="research">
+<section class="engine"><a rel="external" href="https://mobirise.com">simple website maker</a></section><section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height mbr-section--bg-adapted" id="header1-0" style="background-color: rgb(0, 0, 0);">
+    <div class="mbr-box__magnet mbr-box__magnet--sm-padding mbr-box__magnet--center-left mbr-after-navbar">
+        
+        <div class="mbr-box__container mbr-section__container container">
+
+            <div class="mbr-box mbr-box--stretched">
+                <div class="row"><div class=" col-sm-6 col-sm-offset-6">
+                  <div class="mbr-box__magnet mbr-box__magnet--center-left">
+                    <div class="mbr-hero animated fadeInUp">
+                        <h1 class="mbr-hero__text">Research<br><span style="font-weight: normal;"></span></h1>
+                        <p class="mbr-hero__subtext"></p>
+                    </div>
+                </div></div>
+            </div></div>
+        </div>
+    </div>
+</section>
+</div>
+<div id="advisory">
+  <section class="engine"><a rel="external" href="https://mobirise.com">simple website maker</a></section><section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height mbr-section--bg-adapted" id="header1-0" style="background-color: rgb(0, 0, 0);">
+    <div class="mbr-box__magnet mbr-box__magnet--sm-padding mbr-box__magnet--center-left mbr-after-navbar">
+        
+        <div class="mbr-box__container mbr-section__container container">
+
+            <div class="mbr-box mbr-box--stretched">
+                <div class="row"><div class=" col-sm-6 col-sm-offset-6">
+                  <div class="mbr-box__magnet mbr-box__magnet--center-left">
+                    <div class="mbr-hero animated fadeInUp">
+                        <h1 class="mbr-hero__text">Advisory<br><span style="font-weight: normal;"></span></h1>
+                        <p class="mbr-hero__subtext"></p>
+                    </div>
+                </div></div>
+            </div></div>
+        </div>
+    </div>
+</section>
+</div>
+<div id="engagement">
+  <section class="engine"><a rel="external" href="https://mobirise.com">simple website maker</a></section><section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height mbr-section--bg-adapted" id="header1-0" style="background-color: rgb(0, 0, 0);">
+    <div class="mbr-box__magnet mbr-box__magnet--sm-padding mbr-box__magnet--center-left mbr-after-navbar">
+        
+        <div class="mbr-box__container mbr-section__container container">
+
+            <div class="mbr-box mbr-box--stretched">
+                <div class="row"><div class=" col-sm-6 col-sm-offset-6">
+                  <div class="mbr-box__magnet mbr-box__magnet--center-left">
+                    <div class="mbr-hero animated fadeInUp">
+                        <h1 class="mbr-hero__text">Engagement<br><span style="font-weight: normal;"></span></h1>
+                        <p class="mbr-hero__subtext"></p>
+                    </div>
+                </div></div>
+            </div></div>
+        </div>
+    </div>
+</section>
+</div>
+<div id="students">
+  <section class="engine"><a rel="external" href="https://mobirise.com">simple website maker</a></section><section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height mbr-section--bg-adapted" id="header1-0" style="background-color: rgb(0, 0, 0);">
+    <div class="mbr-box__magnet mbr-box__magnet--sm-padding mbr-box__magnet--center-left mbr-after-navbar">
+        
+        <div class="mbr-box__container mbr-section__container container">
+
+            <div class="mbr-box mbr-box--stretched">
+                <div class="row"><div class=" col-sm-6 col-sm-offset-6">
+                  <div class="mbr-box__magnet mbr-box__magnet--center-left">
+                    <div class="mbr-hero animated fadeInUp">
+                        <h1 class="mbr-hero__text">Students<br><span style="font-weight: normal;"></span></h1>
+                        <p class="mbr-hero__subtext"></p>
+                    </div>
+                </div></div>
+            </div></div>
+        </div>
+    </div>
+</section>
+</div>
+
+  <script src="{{ asset('assets/web/assets/jquery/jquery.min.js')}}"></script>
+  <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+  <script src="{{ asset('assets/smooth-scroll/SmoothScroll.js')}}"></script>
+  <script src="{{ asset('assets/mobirise/js/script.js')}}"></script>
+
+  <script type="text/javascript">
+    $("#research").hide();
+    $("#advisory").hide();
+    $("#engagement").hide();
+    $("#students").hide();
+
+    function researchPage(){
+  
+      $("#logo").hide(500);
+  
+    $("#research").show(1000);
+    }
+
+    function studentsPage(){
+  
+      $("#logo").hide();
+  
+    $("#students").show();
+    }
+    </script>
+
+</body>
+</html>
---
+++ resources/views/index2.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Site made with Mobirise Website Builder v3.9.2, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v3.9.2, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="{{ asset('assets/images/logo.png')}}" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:700,400&amp;subset=cyrillic,latin,greek,vietnamese">
+  <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/animate.css/animate.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/mobirise/css/style.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/mobirise/css/mbr-additional.css')}}')}}" type="text/css">
+  
+  
+  
+</head>
+<title>UCLU CBT</title>
+<body>
+<div id="logo">
+<section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height mbr-section--bg-adapted" id="header2-0" style="background-color: rgb(0, 0, 0);">
+    <div class="mbr-box__magnet mbr-box__magnet--sm-padding mbr-box__magnet--center-left">
+        <div class="mbr-buttons btn-inverse mbr-buttons--left"><a class="mbr-buttons__btn btn btn-lg animated fadeInUp delay btn-link" href="https://mobirise.com">LEARN MORE</a> </div>
+        <div class="mbr-box__container mbr-section__container container">
+            <div class="mbr-box mbr-box--stretched"><div class="mbr-box__magnet mbr-box__magnet--center-left">
+                <div class="row"><div class=" col-sm-6 col-sm-offset-6">
+                    <div class="mbr-hero animated fadeInUp">
+                        <h1 class="mbr-hero__text">BLOCKCHAIN<br>LEADING THE NEW REVOLUTION</h1>
+                        <p class="mbr-hero__subtext"><br><br></p>
+                    </div>
+                </div></div>
+            </div></div>
+        </div>
+        
+    </div>
+</section>
+</div>
+
+  <script src="{{ asset('assets/web/assets/jquery/jquery.min.js')}}"></script>
+  <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+  <script src="{{ asset('assets/smooth-scroll/SmoothScroll.js')}}"></script>
+  <script src="{{ asset('assets/mobirise/js/script.js')}}"></script>
+  
+  
+</body>
+</html>
